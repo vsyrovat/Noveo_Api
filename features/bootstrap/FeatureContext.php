f513e4f4938efa371de87cd6400d19d843d511d7
VS <v@s>
2019-04-29 01:47:15 +0300

    Refactoring: simplify behat context

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -2,9 +2,6 @@
 
 use Behat\Behat\Context\Context;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\KernelInterface;
 
 /**
  * This context class contains the definitions of the steps used by the demo 
@@ -14,21 +11,10 @@ use Symfony\Component\HttpKernel\KernelInterface;
  */
 class FeatureContext implements Context
 {
-    /**
-     * @var KernelInterface
-     */
-    private $kernel;
-
-    /**
-     * @var Response|null
-     */
-    private $response;
-
     private $em;
 
-    public function __construct(KernelInterface $kernel, EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $em)
     {
-        $this->kernel = $kernel;
         $this->em = $em;
     }
 
@@ -46,23 +32,5 @@ class FeatureContext implements Context
     public function rollbackTransaction()
     {
         $this->em->rollback();
-    }
-
-    /**
-     * @When a demo scenario sends a request to :path
-     */
-    public function aDemoScenarioSendsARequestTo(string $path)
-    {
-        $this->response = $this->kernel->handle(Request::create($path, 'GET'));
-    }
-
-    /**
-     * @Then the response should be received
-     */
-    public function theResponseShouldBeReceived()
-    {
-        if ($this->response === null) {
-            throw new \RuntimeException('No response received');
-        }
     }
 }
