138dace17f5da57eafac8073d210ec53250828a3
VS <v@s>
2019-05-06 00:02:22 +0300

    ~ recreate db on testing

--- test.sh
+++ test.sh
@@ -13,8 +13,8 @@ echo "Container ready"
 bin/console -q --env=test swagger:check # Check swagger annotations
 echo "Swagger   OK"
 
-#bin/console doctrine:database:drop --env=test --force -q || true
-#bin/console doctrine:database:create --env=test -q
+bin/console doctrine:database:drop --env=test --force -q || true
+bin/console doctrine:database:create --env=test -q
 bin/console doctrine:migrations:migrate --env=test -q
 echo "Database  ready"
 
