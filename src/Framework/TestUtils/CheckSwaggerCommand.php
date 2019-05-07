24a9d3d4ab697dfe75a2c9314faee1f8f1420357
VS <v@s>
2019-05-07 06:58:42 +0300

    .

--- src/Framework/TestUtils/CheckSwaggerCommand.php
+++ src/Framework/TestUtils/CheckSwaggerCommand.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace App\Tests\Helpers;
+namespace App\Framework\TestUtils;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
